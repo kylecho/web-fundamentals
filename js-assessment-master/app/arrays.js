1c29f00473ae797055945e4a1e4cf01797be613b
Kyle Cho <kylecho.work@gmail.com>
2015-06-04 21:17:30 -0700

    Add js-assessment

---
+++ js-assessment-master/app/arrays.js
@@ -0,0 +1,60 @@
+exports = (typeof window === 'undefined') ? global : window;
+
+exports.arraysAnswers = {
+
+  indexOf : function(arr, item) {
+
+  },
+
+  sum : function(arr) {
+
+  },
+
+  remove : function(arr, item) {
+
+  },
+
+  removeWithoutCopy : function(arr, item) {
+
+  },
+
+  append : function(arr, item) {
+
+  },
+
+  truncate : function(arr) {
+
+  },
+
+  prepend : function(arr, item) {
+
+  },
+
+  curtail : function(arr) {
+
+  },
+
+  concat : function(arr1, arr2) {
+
+  },
+
+  insert : function(arr, item, index) {
+
+  },
+
+  count : function(arr, item) {
+
+  },
+
+  duplicates : function(arr) {
+
+  },
+
+  square : function(arr) {
+
+  },
+
+  findAllOccurrences : function(arr, target) {
+
+  }
+};
